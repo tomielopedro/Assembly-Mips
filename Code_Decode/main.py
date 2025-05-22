729965aca6e9d098c252139c6bdc72e0b2d84caa
Pedro Tomielo <tomielopedro586@gmail.com>
2025-05-22 10:26:04 -0300

    updte: união de todos os arquivos em um único

--- Code_Decode/main.py
+++ Code_Decode/main.py
@@ -3,17 +3,20 @@ from utils import get_file_instructions
 
 instrucoes_asm = get_file_instructions('data/asm.txt')
 instrucoes_hexa = get_file_instructions('data/hexa.txt')
-opcode_i = get_json_data('data/opcode_i.json')
-opcode_j = get_json_data('data/opcode_j.json')
-funct_r = get_json_data('data/funct_r.json')
-registers = get_json_data('data/registers.json')
+
+mips_dict = get_json_data('data/mips.json')
+registers = mips_dict.get('registers')
+instructions_i = mips_dict.get('instructions_i')
+instructions_r = mips_dict.get('instructions_r')
+instructions_j = mips_dict.get('instructions_j')
+
 
 print(instrucoes_asm)
 print(instrucoes_hexa)
 
-print(opcode_i)
-print(opcode_j)
-print(funct_r)
+print(instructions_i)
+print(instructions_j)
+print(instructions_r)
 print(registers)
 
 
