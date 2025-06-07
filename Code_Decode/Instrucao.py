6a439d8b13bb4ed5ee03f18f8d0e623198006e10
Pedro Tomielo <tomielopedro586@gmail.com>
2025-06-07 17:30:33 -0300

    add: decodificacao asm para hexadecimal

--- Code_Decode/Instrucao.py
+++ Code_Decode/Instrucao.py
@@ -5,8 +5,9 @@ class InstrucaoMIPS(ABC):
     """
     Classe abstrata base para instruções MIPS.
     """
-    def __init__(self, nome: str):
+    def __init__(self, nome: str, tipo: str):
         self.nome = nome
+        self.tipo = tipo
 
     @abstractmethod
     def binary_format(self) -> str:
@@ -15,13 +16,18 @@ class InstrucaoMIPS(ABC):
         """
         pass
 
+    def hexa_format(self) -> str:
+        return hex(int(self.binary_format(), 2))[2:].zfill(8)
+
+    def __str__(self):
+        return f'Função: {self.nome} Tipo: {self.tipo}'
 
 # ---------------------- Tipo R ----------------------
 
 
 class InstrucaoTipoR(InstrucaoMIPS):
     def __init__(self, nome, rs, rt, rd, shamt, funct):
-        super().__init__(nome)
+        super().__init__(nome, 'R')
         self.opcode = "000000"
         self.rs = rs
         self.rt = rt
@@ -29,44 +35,69 @@ class InstrucaoTipoR(InstrucaoMIPS):
         self.shamt = shamt
         self.funct = funct
 
-    def encode(self) -> str:
-        return f"{self.opcode}{self.rs:05b}{self.rt:05b}{self.rd:05b}{self.shamt:05b}{self.funct}"
+    def binary_format(self) -> str:
+        return f"{self.opcode}{self.rt:05b}{self.rd:05b}{self.rs:05b}{self.shamt:05b}{self.funct:06b}"
+
+    def __str__(self):
+        return f'NOME: {self.nome} OPCODE: {self.opcode}; RS: {self.rs}; RT: {self.rt}; RD {self.rd} SHAMPT: {self.shamt} FUNCT {self.funct}'
+
+
+
 
 # ---------------------- Tipo I ----------------------
 
 
 class InstrucaoTipoI(InstrucaoMIPS):
-    def __init__(self, nome, opcode, rs, rt, immediate):
-        super().__init__(nome)
+    def __init__(self, nome, opcode, rt, rs, immediate):
+        super().__init__(nome, 'I')
+        self.nome = nome
         self.opcode = opcode
         self.rs = rs
         self.rt = rt
         self.immediate = immediate
 
-    def encode(self) -> str:
-        return f"{self.opcode}{self.rs:05b}{self.rt:05b}{self.immediate:016b}"
+    def binary_format(self) -> str:
+        # Gera o binário do campo immediate em complemento de 2
+        if self.immediate < 0:
+            imm_val = (1 << 16) + self.immediate
+        else:
+            imm_val = self.immediate
+        imm_bin = f"{imm_val:016b}"
+
+        return f"{self.opcode:06b}{self.rt:05b}{self.rs:05b}{imm_bin}"
+    def __str__(self):
+        return f'NOME: {self.nome} OPCODE: {self.opcode}; RS: {self.rs}; RT: {self.rt}; IMEDIATE: {self.immediate}'
+
+
+
 
 # ---------------------- Tipo J ----------------------
 
 
 class InstrucaoTipoJ(InstrucaoMIPS):
     def __init__(self, nome, opcode, address):
-        super().__init__(nome)
+        super().__init__(nome, 'J')
         self.opcode = opcode
         self.address = address
 
-    def encode(self) -> str:
-        return f"{self.opcode}{self.address:026b}"
+    def binary_format(self) -> str:
+        adress = self.address + 0x00400000
+        adress = f'{adress:032b}'
+        adress = adress[4:30]
+        return f"{self.opcode:06b}{adress}"
+
+    def __str__(self):
+        return f'NOME: {self.nome} OPCODE: {self.opcode}; ADDRESS: {self.address}'
 
 if __name__ == "__main__":
     # ADD R-type: add $s1, $s2, $s3
-    add_instr = InstrucaoTipoR("add", rs=18, rt=19, rd=17, shamt=0, funct="100000")
-    print("ADD:", add_instr.encode())
+    add_instr = InstrucaoTipoR("add", rs=18, rt=19, rd=17, shamt=0, funct=10)
+    print("ADD:", add_instr.binary_format())
 
     # LW I-type: lw $t0, 4($s3)
     lw_instr = InstrucaoTipoI("lw", opcode="100011", rs=19, rt=8, immediate=4)
-    print("LW:", lw_instr.encode())
+    print("LW:", lw_instr.binary_format())
 
     # J J-type: j 0x004000
     j_instr = InstrucaoTipoJ("j", opcode="000010", address=0x004000)
-    print("J:", j_instr.encode())
+    print("J:", j_instr.binary_format())
--- Code_Decode/main.py
+++ Code_Decode/main.py
@@ -1,7 +1,9 @@
 from utils import get_json_data
 from utils import get_file_instructions
+from Instrucao import *
 
 instrucoes_asm = get_file_instructions('data/asm.txt')
+instrucoes_asm = [instruct.replace(',', '').split(' ') for instruct in instrucoes_asm]
 instrucoes_hexa = get_file_instructions('data/hexa.txt')
 
 mips_dict = get_json_data('data/mips.json')
@@ -10,13 +12,123 @@ instructions_i = mips_dict.get('instructions_i')
 instructions_r = mips_dict.get('instructions_r')
 instructions_j = mips_dict.get('instructions_j')
 
-
 print(instrucoes_asm)
 print(instrucoes_hexa)
-
 print(instructions_i)
 print(instructions_j)
 print(instructions_r)
 print(registers)
+
+
+labels_dict = {
+    instrucao[0].rstrip(':'): i
+    for i, instrucao in enumerate(instrucoes_asm)
+    if instrucao[0].endswith(':')
+}
+
+
+def calcular_deslocamento(start, end, label):
+    deslocamneto = 0
+    for i in range(start, end):
+        linha = instrucoes_asm[i]  # linha do código
+        instrucao = linha[0]  # primeiro elemento da linha instrucao
+
+        if instrucao.startswith(label):
+            instrucao = linha[1]  # a instrucao recebe o segundo elemento da lista caso o primeiro seja o label
+        if instrucao == 'la' or instrucao == 'li':  # verifica se é a instrucao sw ou lw pq ela incrementa 2 no deslocamento
+            deslocamneto += 2
+            continue  # avança pr
+        deslocamneto += 1
+    return deslocamneto
+
+
+
+
+print(labels_dict)
+print('========================')
+
+for c, instrucoes in enumerate(instrucoes_asm):
+
+    for i, instrucao in enumerate(instrucoes):
+        if instrucao in instructions_r.keys():
+            opcode = "000000"
+            shampt = 0
+            print('========================')
+            if instrucoes[i+1] in registers.keys():
+                print(instrucoes[i+1])
+                rs = registers[instrucoes[i+1]]
+            if instrucoes[i+2] in registers.keys():
+                print(instrucoes[i + 2])
+                rt = registers[instrucoes[i+2]]
+            if instrucoes[i+3] in registers.keys():
+                print(instrucoes[i + 3])
+                rd = registers[instrucoes[i+3]]
+            else:
+                if instrucoes[i + 3].isdigit():
+                    rt, rd = 0, rt
+                    shampt = int(instrucoes[i + 3])
+
+            function = instructions_r[instrucao]
+            inst = InstrucaoTipoR(instrucao, rs, rt, rd, shampt, function)
+            binario = inst.binary_format()
+            hexa = inst.hexa_format()
+            print(inst)
+            print(f'Binario: {binario} Tamanho -> {len(binario)}')
+            print('Hexadecimal: ', hexa)
+
+        if instrucao in instructions_i.keys():
+
+            opcode = instructions_i[instrucao]
+            print('========================')
+            if instrucoes[i+1] in registers.keys():
+                rs = registers[instrucoes[i+1]]
+            if instrucoes[i+2] in registers.keys():
+                rt = registers[instrucoes[i+2]]
+                if instrucoes[i + 3].isdigit():
+                    imediate = int(instrucoes[i + 3])
+                else:
+                    if instrucoes[i + 3] in labels_dict.keys():
+
+                        pos_label = labels_dict[instrucoes[i + 3]] # calcular o endereco
+                        if c > pos_label:
+                            deslocamento = calcular_deslocamento(pos_label, c+1, instrucoes[i + 3])
+                            deslocamento *= -1
+                        else:
+                            deslocamento = calcular_deslocamento(c+2, pos_label+1, instrucoes[i + 3])
+                        print('deslocamento final:', deslocamento)
+                        imediate = deslocamento
+                        rs, rt = rt, rs # inverte a ordem se bor condicional
+            else:
+                if instrucoes[i+2][0].isdigit():
+                    rt = registers[instrucoes[i+2][1:].replace('(', '').replace(')', '')]
+                    imediate = int(instrucoes[i+2][0])
+
+            inst = InstrucaoTipoI(nome=instrucao, opcode=opcode, rt=rt, rs=rs,immediate=imediate)
+
+            binario = inst.binary_format()
+            hexa = inst.hexa_format()
+            print(inst)
+            print('Binario: ', binario)
+            print('Hexadecimal: ', hexa)
+            print(f'Binario: {binario} Tamanho -> {len(binario)}')
+
+        if instrucao in instructions_j.keys():
+            opcode = instructions_j[instrucao]
+            print(instrucao)
+            if instrucoes[i+1] in labels_dict.keys():
+                print(c)
+                label_pos = labels_dict[instrucoes[i+1]]
+                adress = calcular_deslocamento(1, label_pos, instrucoes[i+1])*4
+                instruct_j = InstrucaoTipoJ(instrucao, opcode, adress)
+
+                print(instruct_j.binary_format())
+                print(instruct_j.hexa_format())
+
+
+
+print(labels_dict)
+
+
+
 
 
